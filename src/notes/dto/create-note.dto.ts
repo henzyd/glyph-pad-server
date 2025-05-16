543891d8077951c5189f3c458f2249a1a31dd750
henzyd <henzydee@gmail.com>
2025-05-16 02:09:14 +0100

    added swagger docs

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class CreateNoteDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @IsUUID()
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsIP, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsIP()
   ip: string;
